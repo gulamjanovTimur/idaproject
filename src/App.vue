e984c82e01131a121c8a3dff92cbddb1d41c7413
Timur <gulamjanov4@gmail.com>
2022-02-05 00:38:46 +0600

    Added CreateProduct

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <CreateProduct />
   </div>
 </template>
 
+<script>
+import CreateProduct from "./components/CreateProduct";
+export default {
+  name: "App",
+  components: {
+    CreateProduct,
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@font-face {
+  font-family: "Source Sans Pro";
+  font-weight: normal;
+  src: url("./assets/fonts/SourceSansPro-Regular.ttf");
 }
-
-#nav {
+* {
+  font-family: Source Sans Pro;
+}
+#app {
   padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  min-height: 100vh;
+  box-sizing: border-box;
+  background-color: #e5e5e5;
+}
+body {
+  margin: 0;
 }
 </style>
---
+++ src/components/CreateProduct.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="create-product">
+    <h1 class="create-product__title">Добавление товара</h1>
+    <form class="create-product__form create-product-form">
+      <div
+        class="create-product-form__label create-product-form__label--required"
+      >
+        Наименование товара
+      </div>
+      <input
+        placeholder="Введите наименование товара"
+        class="create-product-form__input"
+        v-model="form.name"
+      />
+      <div
+        class="create-product-form__label create-product-form__label--required"
+      >
+        Описание товара
+      </div>
+      <textarea
+        placeholder="Введите описание товара"
+        class="create-product-form__area"
+        v-model="form.description"
+      />
+      <div
+        class="create-product-form__label create-product-form__label--required"
+      >
+        Ссылка на изображение товара
+      </div>
+      <input
+        placeholder="Введите ссылку"
+        class="create-product-form__input"
+        v-model="form.linkImg"
+      />
+      <div
+        class="create-product-form__label create-product-form__label--required"
+      >
+        Цена товара
+      </div>
+      <input
+        placeholder="Введите цену"
+        class="create-product-form__input"
+        v-model="form.price"
+      />
+      <button class="create-product-form__btn">Добавить товар</button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  name: "CreateProduct",
+  data() {
+    return {
+      form: {
+        price: "",
+        name: "",
+        description: "",
+        linkImg: "",
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.create-product {
+  &__title {
+    color: #3f3f3f;
+    size: 28px;
+  }
+}
+.create-product-form {
+  width: 332px;
+  padding: 24px;
+  background-color: #fffefb;
+  box-sizing: border-box;
+  border-radius: 4px;
+  &__label {
+    font-size: 10px;
+    color: #49485e;
+    margin-bottom: 4px;
+    position: relative;
+    width: fit-content;
+    &--required {
+      &::after {
+        content: "";
+        display: block;
+        height: 4px;
+        width: 4px;
+        background-color: #ff8484;
+        position: absolute;
+        top: 0;
+        border-radius: 50%;
+        right: -5px;
+      }
+    }
+  }
+  &__input {
+    box-sizing: border-box;
+    padding: 10px 16px;
+    font-size: 12px;
+    height: 36px;
+    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+    border: none;
+    width: 100%;
+    margin-bottom: 16px;
+    border-radius: 4px;
+    color: #3f3f3f;
+    &::placeholder {
+      color: #b4b4b4;
+    }
+  }
+  &__area {
+    padding: 10px 16px;
+    font-size: 12px;
+    border-radius: 4px;
+    border: none;
+    color: #3f3f3f;
+    box-sizing: border-box;
+    width: 100%;
+    height: 108px;
+    resize: none;
+    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+    &::placeholder {
+      color: #b4b4b4;
+    }
+  }
+  &__btn {
+    background-color: #eeeeee;
+    width: 100%;
+    height: 36px;
+    border: none;
+    font-size: 12px;
+    color: #b4b4b4;
+    border-radius: 10px;
+  }
+}
+</style>
